b8a26f7b7206d81cc17cc81a6f948573ef421a6e
ClydeLartey97 <lartey.d@etoncollege.org.uk>
2022-03-03 00:08:42 +0000

    PART 2

---
+++ MusicByLocation/Extensions/CLUPlacemark.swift
@@ -0,0 +1,21 @@
+//
+//  CLUPlacemark.swift
+//  MusicByLocation
+//
+//  Created by Clyde Lartey on 02/03/2022.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLPlacemark {
+    func getLocationBreakdown() -> String {
+        return
+            """
+            Street: \(self.thoroughfare ?? "None")
+            City: \(self.locality ?? "None")
+            Area: \(self.administrativeArea ?? "None")
+            Country: \(self.country ?? "None")
+            """
+    }
+}
